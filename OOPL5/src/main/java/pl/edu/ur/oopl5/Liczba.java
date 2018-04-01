e08273c9649fe39e1bfa09dc72a72bb4d0fe1653
x4nte <x4nte@MSI-PC>
2018-04-01 23:37:38 +0200

    Zadanie 6 gotowe

--- OOPL5/src/main/java/pl/edu/ur/oopl5/Liczba.java
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/Liczba.java
@@ -2,27 +2,61 @@ package pl.edu.ur.oopl5;
 import java.util.Scanner;
 
 public class Liczba {
-    private Integer[] number = new Integer[10];
+    private Integer[] number;
 
-    public Liczba() {
+    public Liczba(String valueString) {
+        number = new Integer[valueString.length()];
+        Character[] znaki = new Character[valueString.length()];
         
+        for (int i=0; i<znaki.length; i++){
+            znaki[i] = valueString.charAt(i);
+        }
+        
+        for (int i=0; i<znaki.length; i++){
+            number[i] = Character.getNumericValue(znaki[i]);
+        }
     }
     
     public void showContent(){
-        for (int i=0; i<number.length-1; i++){
+        for (int i=0; i<number.length; i++){
             System.out.print(number[i]);
         }
         System.out.println();
     }
     
-    public void setValue(String valueString){
-        int tempInt;
-        char tempC;
-        for (int i=0; i<valueString.length()-1;i++){
-            tempC = valueString.charAt(i);
-            //tempInt = Integer.valueOf(tempC);
-            tempInt = Character.getNumericValue(tempC);
-            number[i] = tempInt;
+    public void multi(){
+        Scanner scanner = new Scanner(System.in);
+        String valueString = "";
+        
+        System.out.println("zadlab5 > Menu > Zadanie 6 > Menu > Pomnoz >");
+        System.out.print(" Podaj czynnik: ");
+        int skladnik = scanner.nextInt();
+        
+        for (int i=0; i<number.length; i++){
+            valueString = valueString + Integer.toString(number[i]);
         }
+        
+        int valueInt = Integer.valueOf(valueString);
+        int result = valueInt * skladnik;
+        
+        valueString = Integer.toString(result);
+        
+        Character[] znaki = new Character[valueString.length()];
+        
+        for (int i=0; i<znaki.length; i++){
+            znaki[i] = valueString.charAt(i);
+        }
+        
+        Integer[] resultTable = new Integer[znaki.length];
+        
+        for (int i=0; i<znaki.length; i++){
+            resultTable[i] = Character.getNumericValue(znaki[i]);
+        }
+        
+        System.out.print("Wynik mnozenia: ");
+        for (int i=0; i<resultTable.length; i++){
+            System.out.print(resultTable[i]);
+        }
+        System.out.println();
     }
 }
--- OOPL5/src/main/java/pl/edu/ur/oopl5/Zadanie6.java
+++ OOPL5/src/main/java/pl/edu/ur/oopl5/Zadanie6.java
@@ -2,41 +2,56 @@ package pl.edu.ur.oopl5;
 import java.util.Scanner;
 
 public class Zadanie6 {
-    Liczba liczba = new Liczba();
+    Liczba liczba;
     
     public void showContent(){
         liczba.showContent();
     }
     
-    public void setValue(){
+    private void setValue(){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("zadlab5 > Menu > Zadanie 6 > Menu > Nadaj wartosc >");
+        System.out.println("zadlab5 > Menu > Zadanie 6 > Nadaj wartosc >");
         System.out.print(" Podaj wartosc: ");
         String value = scanner.nextLine();
-        liczba.setValue(value);
+        
+        liczba = new Liczba(value);
     }
     
-    public void multi(){
+    private void factorialStart(){
+        Scanner scanner = new Scanner(System.in);
         
+        System.out.println("zadlab5 > Menu > Zadanie 6 > Menu > Silnia >");
+        System.out.print(" Podaj liczbe, z ktore ma zostac policzona silnia: ");
+        int n = scanner.nextInt();
+        System.out.println("Silnia z " + n + " jest rowna " + factorial(n));
+    }
+    
+    private long factorial(int n){
+        long result = 1;
+        for (int i=1; i<=n; i++){
+            result = result * i;
+        }
+        return result;
     }
     
     public void menu(){
         Scanner scanner = new Scanner(System.in);
         int menu = 1;
         
+        setValue();
         while(menu !=0){
             System.out.println("zadlab5 > Menu > Zadanie 6 > Menu >");
             System.out.println(" [ 1 ] Wypisz");
-            System.out.println(" [ 2 ] Nadaj wartosc");
-            System.out.println(" [ 3 ] Pomnoz");
+            System.out.println(" [ 2 ] Pomnoz");
+            System.out.println(" [ 3 ] Silnia");
             System.out.println(" [ 0 ] Cofnij");
             System.out.print("Menu > ");
             menu = scanner.nextInt();
             
             switch(menu){
                 case 1: showContent(); break;
-                case 2: setValue(); break;
-                case 3: multi(); break;
+                case 2: liczba.multi(); break;
+                case 3: factorialStart(); break;
                 case 0: break;
                 default: System.out.println("! Nieprawidlowa opcja");
             }
